d25b845b00c2acf8cdfac9f50f49bf53625f28f5
vinayak yadav <94183058+vinayaky@users.noreply.github.com>
2026-06-11 16:46:58 +0530

    Update datadimansion.py

--- datadimansion.py
+++ datadimansion.py
@@ -8,7 +8,7 @@ from langchain.agents import initialize_agent, Tool,AgentType
 
 
 os.environ['OPENAI_API_KEY'] = apikey
-os.environ['SERPAPI_API_KEY']='2c0b2505e505644a2c43da7a76055c48344c7cf4078f5f09b34fb1e6a60555e8'
+os.environ['SERPAPI_API_KEY']=serpapi
 
 
 prompt=input('Enter your prompt: ')
